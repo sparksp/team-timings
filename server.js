89db92474319a75b9a4af60926879d1596814e1a
Phill Sparks <me@phills.me.uk>
2016-10-17 21:54:42 +0100

    Adds Procfile and reads port from env

--- server.js
+++ server.js
@@ -4,9 +4,10 @@ var express = require('express');
 var teamTimings = require('./routes/team-timings');
 
 var app = express();
+app.set('port', (process.env.PORT || 5000));
 app.use(express.static('public'));
 app.use('/', teamTimings);
 
-app.listen(3000, () => {
-  console.info('Team Timings listening on port 3000');
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
 });
